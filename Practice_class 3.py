2a87c2fc6f3bb2789b0a401c69fcd3a290c40d55
robinmillican <robin.millican@duke.edu>
2015-01-28 15:25:03 -0500

    Practicing Python - Class 3

---
+++ Practice_class 3.py
@@ -0,0 +1,6 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+main_dir = "/Users/robin/Desktop/Big Data/Data"
+git_dir = "/Users/robin/Desktop/Data for PUBPOL590/"
